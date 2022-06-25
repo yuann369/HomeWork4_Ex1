b57ea6556208b31f7e687ca0817afe87fbdd9da8
yuann369 <ayurasova369@gmail.com>
2022-06-25 20:00:23 +0400

    changes for home work 6

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,63 +1,75 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class BonusServiceTest {
 
-    @Test
-    public void shouldCalculateForRegisteredAndUnderLimit() {
+    @CsvFileSource(resources = {"/data.csv"})
+    @ParameterizedTest
+    public void shouldCalculate(String user, long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculateForRegisteredAndOverLimit() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 1_000_000_60;
-        boolean registered = true;
-        long expected = 500;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculateForNotRegisteredAndUnderLimit() {
-        BonusService service = new BonusService();
-
-        long amount = 1000_60;
-        boolean registered = false;
-        long expected = 10;
 
         long actual = service.calculate(amount, registered);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void shouldCalculateForNotRegisteredAndOverLimit() {
-        BonusService service = new BonusService();
-
-        long amount = 1_000_000_60;
-        boolean registered = false;
-        long expected = 500;
-
-        long actual = service.calculate(amount, registered);
-
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    public void shouldCalculateForRegisteredAndUnderLimit() {
+//        BonusService service = new BonusService();
+//
+//        // подготавливаем данные:
+//        long amount = 1000_60;
+//        boolean registered = true;
+//        long expected = 30;
+//
+//        // вызываем целевой метод:
+//        long actual = service.calculate(amount, registered);
+//
+//        // производим проверку (сравниваем ожидаемый и фактический):
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void shouldCalculateForRegisteredAndOverLimit() {
+//        BonusService service = new BonusService();
+//
+//        // подготавливаем данные:
+//        long amount = 1_000_000_60;
+//        boolean registered = true;
+//        long expected = 500;
+//
+//        // вызываем целевой метод:
+//        long actual = service.calculate(amount, registered);
+//
+//        // производим проверку (сравниваем ожидаемый и фактический):
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void shouldCalculateForNotRegisteredAndUnderLimit() {
+//        BonusService service = new BonusService();
+//
+//        long amount = 1000_60;
+//        boolean registered = false;
+//        long expected = 10;
+//
+//        long actual = service.calculate(amount, registered);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void shouldCalculateForNotRegisteredAndOverLimit() {
+//        BonusService service = new BonusService();
+//
+//        long amount = 1_000_000_60;
+//        boolean registered = false;
+//        long expected = 500;
+//
+//        long actual = service.calculate(amount, registered);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
